acddcde0d49805c5ad3cce753d959ae522df5c48
Talha <mtalhaasifshazad@gmail.com>
2023-03-20 21:19:44 +0500

    Text to speech

---
+++ Assistant.py
@@ -0,0 +1,12 @@
+import pyttsx3
+import datetime
+
+engine =pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[0].id)
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+if __name__ == "__main__":
+    speak("Hello World!")
