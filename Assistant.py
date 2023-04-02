ff8e09f60584962dd132b656a3dcd7b3cffd35a4
Talha <mtalhaasifshazad@gmail.com>
2023-04-02 17:13:19 +0500

    I add some more functionality in this program

--- Assistant.py
+++ Assistant.py
@@ -3,6 +3,7 @@ import datetime
 import speech_recognition as sr
 import wikipedia
 import webbrowser
+import os
 
 
 engine =pyttsx3.init('sapi5')
@@ -30,7 +31,7 @@ def voice_commands_to_text():
     rec_class = sr.Recognizer()
     with sr.Microphone() as source:
         print("listning.....")
-        rec_class.pause_threshold = 1
+        rec_class.pause_threshold = 0.5
         rec_class.energy_threshold = 450
         audio = rec_class.listen(source)
     try:
@@ -45,7 +46,7 @@ def voice_commands_to_text():
 
 
 if __name__ == "__main__":
-    # greeting()
+    greeting()
     while True:
         command  = voice_commands_to_text().lower()
         if 'wikipedia' in command:
@@ -57,9 +58,17 @@ if __name__ == "__main__":
             speak(results)
         elif 'open google' in command:
             webbrowser.open('google.com')
+
         elif 'open youtube ' in command:
             webbrowser.open('https://www.youtube.com/')
+
         elif 'in youtube' in command:
             se = command.split(' in ')[0]
             webbrowser.open(f'https://www.youtube.com/results?search_query={se}')
+
+        elif 'current time' in command:
+            speak("The time is" + datetime.datetime.now().strftime('%H:%M:%S'))
         
+        elif 'open code' in command:
+            Path = "C:\\Microsoft VS Code\\Code.exe"
+            os.startfile(Path)
